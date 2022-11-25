e288ccf558fb4e8f6f35b712aecd621d1428911a
RubenAcosta16 <1511ruben@gmail.com>
2022-11-24 18:57:51 -0700

    first commit

---
+++ js/menu.js
@@ -0,0 +1,32 @@
+const menu=document.querySelector(".menu");
+
+const openMenu=document.querySelector(".open-menu");
+const closeMenu=document.querySelector(".close-menu");
+
+
+
+openMenu.addEventListener("click",menuToggle);
+closeMenu.addEventListener("click",menuToggle);
+
+
+
+function menuToggle(){
+    menu.classList.toggle("menu_opened")
+}
+
+
+
+
+const menuLinks=document.querySelectorAll(".menu a[href^='#']");
+
+menuLinks.forEach(menuLink =>{
+    menuLink.addEventListener("click", function(){
+        menu.classList.remove("menu_opened");
+    });
+
+    const hash=menuLink.getAttribute("href");
+    const target=document.querySelector(hash);
+    if(target){
+        observer.observe(target);
+    }
+});
