ac8ee966011d975dd9e4411c5549f9c3f8c09714
RubenAcosta16 <1511ruben@gmail.com>
2022-11-27 16:13:06 -0700

    more things fixed

--- js/conversor.js
+++ js/conversor.js
@@ -6,7 +6,7 @@ let conversor_1=document.querySelector(".conversor-1");
 let conversor_2=document.querySelector(".conversor-2");
 let conversor_3=document.querySelector(".conversor-3");
 
-
+let conversor_tutorial=document.querySelectorAll(".conversor-tutorial");
 
 
 binario_decimal.addEventListener("click",()=>{
@@ -19,6 +19,11 @@ binario_decimal.addEventListener("click",()=>{
 
     conversor_3.classList.remove("conversor-on");
     conversor_3.classList.add("conversor-off");
+
+
+    conversor_tutorial[0].setAttribute("id","tutorial");
+    conversor_tutorial[1].removeAttribute("id");
+    conversor_tutorial[2].removeAttribute("id");
 
 
 });
@@ -34,6 +39,10 @@ decimal_hexadecimal.addEventListener("click",()=>{
     conversor_3.classList.add("conversor-off");
 
 
+    conversor_tutorial[0].removeAttribute("id");
+    conversor_tutorial[1].setAttribute("id","tutorial");
+    conversor_tutorial[2].removeAttribute("id");
+
 });
 hexadecimal_binario.addEventListener("click",()=>{
     conversor_1.classList.remove("conversor-on");
@@ -46,6 +55,10 @@ hexadecimal_binario.addEventListener("click",()=>{
     conversor_3.classList.remove("conversor-off");
     conversor_3.classList.add("conversor-on");
 
+
+    conversor_tutorial[0].removeAttribute("id");
+    conversor_tutorial[1].removeAttribute("id");
+    conversor_tutorial[2].setAttribute("id","tutorial");
 
 });
 
@@ -62,6 +75,8 @@ const op2=document.querySelectorAll(".op2");
 let conversor_op1=document.querySelectorAll(".conversor-op1");
 let conversor_op2=document.querySelectorAll(".conversor-op2");
 
+
+
 for(let i=0;i<3;i++){
     op1[i].addEventListener("click",()=>{
         conversor_op1[i].classList.remove("conversor-off");
@@ -74,14 +89,14 @@ for(let i=0;i<3;i++){
     });
 
     op2[i].addEventListener("click",()=>{
-    conversor_op1[i].classList.remove("conversor-on");
-    conversor_op1[i].classList.add("conversor-off");
+        conversor_op1[i].classList.remove("conversor-on");
+        conversor_op1[i].classList.add("conversor-off");
 
-    conversor_op2[i].classList.remove("conversor-off");
-    conversor_op2[i].classList.add("conversor-on");
+        conversor_op2[i].classList.remove("conversor-off");
+        conversor_op2[i].classList.add("conversor-on");
 
-    console.log("op2");
-});
+        console.log("op2");
+    });
 }
 
 
