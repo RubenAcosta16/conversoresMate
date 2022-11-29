85289a5eb314e4f9cf933e6cec0d952c52e5ac8b
RubenAcosta16 <1511ruben@gmail.com>
2022-11-28 21:58:59 -0700

    all finished

--- js/conversor-app.js
+++ js/conversor-app.js
@@ -15,16 +15,7 @@ window.addEventListener("load",function(){
     let conversor_ans = document.querySelectorAll(".conversor-ans");
     
     
-    // for(let i=0;i<input_conversor.length;i++){
-    //     input_conversor[i].addEventListener("input",function(){
-    //         let num=parseInt(input_conversor[i].value);
 
-    //         conversor_ans[i].innerHTML= num*2;
-            
-    //         // console.log(typeof num);
-    //         // console.log(num);
-    //     });
-    // }
 
 
             input_conversor[0].addEventListener("input",function(){
@@ -35,9 +26,6 @@ window.addEventListener("load",function(){
                     conversor_ans[0].innerHTML= num;
                 
                 
-                
-                // console.log(typeof num);
-                // console.log(num);
             });
 
             
@@ -76,7 +64,7 @@ window.addEventListener("load",function(){
                
                
                
-            //    console.log(hexaToDec("1f"));
+
                
             input_conversor[4].addEventListener("input",function(){
 
@@ -98,15 +86,14 @@ window.addEventListener("load",function(){
     
                 conversor_ans[5].innerHTML= binToHexa(num,2,16);
                 
-                // console.log(typeof num);
-                // console.log(num);
+
             });
 
             
-    // email.addEventListener("change",envioAutomatico);
+
 });
 
-// 2
+// 1
 function decToBin (number) {
     let num = number;
     let binary = (num % 2).toString();
@@ -116,30 +103,24 @@ function decToBin (number) {
     }
     return binary;
 }
-// 3
+// 2
 function decToHex(n){
     return n.toString(16).toUpperCase()
 }
 
-// 4
+// 3
 function hexaToDec(n){
     let dec2 = parseInt(n,16);
     //console.log("Convertido a hexadecimal es: " +dec2);
    return dec2;
    }
 
-//    function hexaToBin(n){
-//     let decimal=parseInt(n,16); 
-
-//     return decimal.
-//    }
+// 4
 function hexaToBin(hex){
     return (parseInt(hex, 16).toString(2)).padStart(4);
 }
 
-// function binToHexa(num){
-//     Number(parseInt(num,2)).toString(16);
-// }
+
 
 function binToHexa(value,sourceBase,targetBase){
     return Number(parseInt(value,sourceBase)).toString(targetBase).toUpperCase();
